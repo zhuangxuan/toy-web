cae32838973c78336ead9650b982b07fc09d0778
Andy-庄绪安 <790280811@qq.com>
2024-04-18 18:36:36 +0800

    qqqddd

--- test.go
+++ test.go
@@ -1,13 +1,17 @@
 package main
 
-import "fmt"
-
-func foo() *int {
-	t := 3
-	return &t
-}
+import (
+	"fmt"
+)
 
 func main() {
-	x := foo()
-	fmt.Println(*x)
+	s := make([]int, 0, 1)
+	c := cap(s)
+	for i := 0; i < 1000; i++ {
+		s = append(s, i)
+		if n := cap(s); n > c {
+			fmt.Printf("cap: %d -> %d\n", c, n)
+			c = n
+		}
+	}
 }
