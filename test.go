988c2be5f62041f5c0f4ac3e5fb74ddd7ce02eaa
Andy-庄绪安 <790280811@qq.com>
2024-04-19 09:32:28 +0800

    qqqddd

--- test.go
+++ test.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	s := make([]int, 0, 1)
-	c := cap(s)
-	for i := 0; i < 1000; i++ {
-		s = append(s, i)
-		if n := cap(s); n > c {
-			fmt.Printf("cap: %d -> %d\n", c, n)
-			c = n
-		}
+	ch := make(chan int)
+	for i := 0; i < 10; i++ {
+		go func(i int) {
+			// 处理数据...
+			result := i
+			ch <- result
+			fmt.Println("Goroutine", i, "finished")
+		}(i)
+	}
+	for i := 0; i < 10; i++ {
+		time.Sleep(1 * time.Second)
+		fmt.Printf("Result: %d\n", <-ch)
+	}
+	for {
+
 	}
 }
